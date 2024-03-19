7734f04572fe656082e9d01bfeebcb93500eb5f6
Jon Roberts <jon.roberts.8@gmail.com>
2024-03-19 15:16:23 -0400

    my plural solution

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -64,16 +64,15 @@ class Bottles
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "#{number-1} bottles of beer on the wall.\n"
+      "#{number-1} #{bottle_inflection(number -1)} of beer on the wall.\n"
     end
+  end
+
+  def bottle_inflection(count)
+    count == 1 ? "bottle" : "bottles"
   end
 end
