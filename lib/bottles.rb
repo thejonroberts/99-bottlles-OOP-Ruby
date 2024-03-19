01d348b6491af88743ea4b1834ed0a25a09d843c
Jon Roberts <jon.roberts.8@gmail.com>
2024-03-19 17:55:37 -0400

    number/count -> bottle_number

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,42 +4,42 @@
 #   end
 
 #   def verses(high, low)
-#     array = high.downto(low).map do |count|
-#       verse(count)
+#     array = high.downto(low).map do |number|
+#       verse(number)
 #     end
 #     array.join("\n")
 #   end
 
-#   def verse(count)
-#     phrase_one(count) + phrase_two(count - 1)
+#   def verse(number)
+#     phrase_one(number) + phrase_two(number - 1)
 #   end
 
-#   def phrase_one(count)
-#     inflection = inflection(count)
-#     number = number_phrase(count)
+#   def phrase_one(number)
+#     inflection = inflection(number)
+#     number = number_phrase(number)
 #     "#{number.to_s.capitalize} #{inflection} of beer on the wall, " +
 #       "#{number} #{inflection} of beer.\n"
 #   end
 
-#   def phrase_two(count)
-#     if count >= 0
-#       "Take #{it_check(count)} down and pass it around, " +
-#         "#{number_phrase(count)} #{inflection(count)} of beer on the wall.\n"
+#   def phrase_two(number)
+#     if number >= 0
+#       "Take #{it_check(number)} down and pass it around, " +
+#         "#{number_phrase(number)} #{inflection(number)} of beer on the wall.\n"
 #     else
 #       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
 #     end
 #   end
 
-#   def number_phrase(count)
-#     count == 0 ? "no more" : count
+#   def number_phrase(number)
+#     number == 0 ? "no more" : number
 #   end
 
-#   def inflection(beer_count)
-#     beer_count == 1 ? "bottle" : "bottles"
+#   def inflection(beer_number)
+#     beer_number == 1 ? "bottle" : "bottles"
 #   end
 
-#   def it_check(count)
-#     count == 0 ? "it" : "one"
+#   def it_check(number)
+#     number == 0 ? "it" : "one"
 #   end
 # end
 
@@ -59,40 +59,40 @@ class Bottles
     "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
   end
 
-  def successor(number)
-    if number == 0
+  def successor(bottle_number)
+    if bottle_number == 0
       99
     else
-      number - 1
+      bottle_number - 1
     end
   end
 
-  def action(number = :FIXME)
-    if number == 0
+  def action(bottle_number = :FIXME)
+    if bottle_number == 0
       "Go to the store and buy some more"
     else
-      "Take #{pronoun(number)} down and pass it around"
+      "Take #{pronoun(bottle_number)} down and pass it around"
     end
   end
 
-  def quantity(count = 0)
-    if count == 0
+  def quantity(bottle_number = 0)
+    if bottle_number == 0
       "no more"
     else
-      count.to_s
+      bottle_number.to_s
     end
   end
 
-  def container(count)
-    if count == 1
+  def container(bottle_number)
+    if bottle_number == 1
       "bottle"
     else
       "bottles"
     end
   end
 
-  def pronoun(count = :FIXME)
-    if count == 1
+  def pronoun(bottle_number = :FIXME)
+    if bottle_number == 1
       'it'
     else
       'one'
